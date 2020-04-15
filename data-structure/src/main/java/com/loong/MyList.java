f7af46ab59fc5f2d1cb9d38c99b29f7cd5e7f8e6
yvanme <842323064@qq.com>
2020-04-15 17:44:12 +0800

    leetcode

---
+++ data-structure/src/main/java/com/loong/MyList.java
@@ -0,0 +1,23 @@
+package com.loong;
+
+/**
+ * @author yvanme
+ * @version 1.0
+ * @Description
+ * @create 2020-04-15 17:42:37
+ **/
+public class MyList {
+
+    public void add(){
+
+    }
+    public void get(){
+
+    }
+    public void del(){
+
+    }
+    public void upd(){
+
+    }
+}
